49a20d532cf258d21ddabe5d0199ecf0d72f9908
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-20 13:53:12 +0100

    Created a custom hook to fetch movies on the Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,33 +6,12 @@ import NoImage from '../images/no_image.jpg';
 
 import API from '../API';
 
+import { useHomeFetch } from '../hooks/useHomeFetch';
+
 const Home = () => {
-    const [ state, setState ] = useState();
-    const [ loading, setLoading ] = useState(false);
-    const [error, setError ] = useState(false);
 
-
-    const fetchMovies = async (page, searchTerm = "" ) => {
-            try {
-                setError(false);
-                setLoading(true );
-
-                const movies = await API.fetchMovies(searchTerm, page);
-                setState(prev => ({
-                    ...movies,
-                    results: page > 1 ? [...prev.results, ...movies.results ]: [...movies.results]
-
-                }));
-            } catch (error) {
-                setError(true);
-            }
-            setLoading(false);
-    }
-
-    useEffect(() => {
-        fetchMovies(1)
-    }, []);
-
+    const { state, loading, error } = useHomeFetch();
+    
     console.log(state);
 
     return (
