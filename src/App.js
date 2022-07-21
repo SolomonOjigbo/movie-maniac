97d1501722ad0d0a944ca14ee103dd1fd051f767
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-21 18:42:38 +0100

    Setup the Routes to the Home, Movie and NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+
+import { BrowserRouter,  Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
-import Home from './components/Home'
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 import { GlobalStyle } from './GlobalStyle';
 
 const App = () =>  ( 
-        <div >
+        <BrowserRouter >
             <Header />
-            <Home />
-            <GlobalStyle />
-        </div>
+            <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/:movieId' element={<Movie />} />
+            <Route path='/*' element={<NotFound />} />
+            </Routes>
+            <GlobalStyle />  
+        </BrowserRouter>
         
         );
 
--- src/components/Thumbnails/index.js
+++ src/components/Thumbnails/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 import { Image } from './Thumbnails.styles';
 
@@ -6,9 +7,14 @@ import { Image } from './Thumbnails.styles';
 
 const Thumbnails = ( props ) => (
     <div>
-    
+    { props.clickable ? (
+    <Link to={`/${props.movieId}`}>
+      <Image src={props.image} alt='movie-thumb' />
+      </Link>
+      ) : (
         <Image src={props.image} alt='movie-thumb' />
-     
+      )
+      } 
   </div>
 )
 
