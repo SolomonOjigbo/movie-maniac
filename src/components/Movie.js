425a1349b19789c6d9c5e19ad9e379b14981c97f
SolomonOjigbo <solomonojigbo@gmail.com>
2022-07-21 19:48:48 +0100

    Configured the useMovieFetch Hook to fetch details for each Movie from TMDB API

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,23 @@
 import React from "react";
 
+import { useParams } from 'react-router-dom';
+
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 
+import { useMovieFetch } from '../hooks/useMovieFetch';
+
 import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    if (loading) return <Spinner />;
+    if (error) return <div>Something went wrong...</div>;
     
     return (
         <>
